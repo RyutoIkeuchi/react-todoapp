121a83ab9e9bd1739545c9156bbe35883abce759
ryuto <ryu.i7133@gmail.com>
2021-04-28 20:08:31 +0900

    入力ボタンとタスクの表示を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className='text-center mt-8'>こんにちは</h1>
-    </div>
-  );
+	const [text, setText] = useState('');
+	const [tasks, setTasks] = useState([]);
+
+	const onChangeText = (e) => {
+		return setText(e.target.value);
+	};
+
+	const onSubmitButton = () => {
+		setTasks([...tasks,text]);
+		setText('');
+	};
+
+	return (
+		<div className="App">
+			<h1 className="text-center mt-8">こんにちは</h1>
+			<div>
+				<input
+					className="border-solid border border-gray-700"
+					type="text"
+					onChange={onChangeText}
+					value={text}
+				/>
+				<button onClick={onSubmitButton}>送信</button>
+			</div>
+			<ul>
+				{tasks.map((task) => {
+          return (
+            <div key={task}>
+              <li>{task}</li>
+            </div>
+          )
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
